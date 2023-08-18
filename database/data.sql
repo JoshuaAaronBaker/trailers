13a18aa56bfe6a60eb3127a1bd0a9ee995bb2291
JoshuaAaronBaker <jbaker264@yahoo.com>
2023-08-18 02:49:45 +0000

    changed credentials for demo user.

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,2 @@
 insert into "users" ("username", "hashedPassword")
-values ('demo', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA');
+values ('baker', '$argon2id$v=19$m=4096,t=3,p=1$gjuuJMX6zZHijcNKQcoBKg$L1mf2VQTvMDhadWsMSsKHoYTndFemc1NcN6832hTiLI');
